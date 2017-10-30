42f11d0da7c1eea7980079fb459796ff1886beca
sukso96100 <sukso96100@gmail.com>
2017-10-30 18:53:08 +0900

    update

--- jf/src/io/jewelry/finder/MainWindow.java
+++ jf/src/io/jewelry/finder/MainWindow.java
@@ -4,12 +4,16 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
 
 public class MainWindow extends JFrame {
 
 	private JPanel contentPane;
+	private JLabel title;
 
 	/**
 	 * Launch the application.
@@ -35,8 +39,12 @@ public class MainWindow extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		title = new JLabel();
+		title.setText("보석파인더");
+		contentPane.add(title, BorderLayout.NORTH);
 	}
 
 }
