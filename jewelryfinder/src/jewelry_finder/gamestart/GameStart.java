7a4cd1fa302fc24f18d56394226a522d4c9c42b1
gno0929 <gno0929@gmail.com>
2017-11-03 01:18:29 +0900

    UI First Up

---
+++ jewelryfinder/src/jewelry_finder/gamestart/GameStart.java
@@ -0,0 +1,22 @@
+package jewelry_finder.gamestart;
+
+import java.awt.Color;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GameStart extends JPanel {
+	public static final int SCREEN_WIDTH = 1200;
+	public static final int SCREEN_HEIGHT = 720;
+	
+	private JLabel label;
+	
+	public GameStart() {
+		setBackground(Color.BLACK);
+		setLayout(null);
+		
+		label = new JLabel("gamestart?");
+		add(label);
+		setVisible(true);
+	}
+}
